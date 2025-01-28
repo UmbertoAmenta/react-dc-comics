a3fe414f396523712b1151fe3e0e89cc56da9c47
Umberto Amenta <umberto.amenta@hotmail.it>
2025-01-28 18:25:11 +0100

    feat: +header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-import './App.css'
+import Header from "./components/Header";
 
-export default function App() {}
+export default function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
