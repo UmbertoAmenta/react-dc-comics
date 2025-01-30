bd509572ac0def25b1277e8ce666c8cd389b104a
Umberto Amenta <umberto.amenta@hotmail.it>
2025-01-30 19:39:57 +0100

    feat: main: +hero; +dynamic board

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,28 @@
+import comics from "../data/comics";
+
 export default function Main() {
   return (
     <main>
-      <div className="container">
-        <h2> --&gt; Content goes here &lt;--</h2>
-      </div>
+      <section className="hero">
+        <div className="container">
+          <button type="button">current series</button>
+        </div>
+      </section>
+      <section className="board">
+        <div className="container flex">
+          {comics.map((comic) => {
+            return (
+              <div key={comic.id} className="card">
+                <div>
+                  <img src={comic.thumb} alt={comic.series} />
+                </div>
+                <h4>{comic.title}</h4>
+              </div>
+            );
+          })}
+        </div>
+        <button type="button">load more</button>
+      </section>
     </main>
   );
 }
